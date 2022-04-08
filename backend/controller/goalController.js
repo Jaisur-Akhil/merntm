698f1a7e04561795c63e17aa278a3053de024902
Akhil <77776342+Jaisur-Akhil@users.noreply.github.com>
2022-04-08 12:47:25 +0530

    Async Handler + Error (Middleware) Which is still not working + Express- Async-Handler

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,12 +1,11 @@
 /** @format */
+const asyncHandler = require('express-async-handler');
 
-const { error } = require('console');
-
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get Message from controller' });
-};
+});
 
-const setGoals = (req, res) => {
+const setGoals = asyncHandler(async (req, res) => {
   console.log(req.body);
   if (!req.body.brain) {
     res.status(400);
@@ -14,15 +13,15 @@ const setGoals = (req, res) => {
     // res.status(400).json({ message: 'please text not brain' });
   }
   res.status(200).json({ Message: 'Set Goal' });
-};
+});
 
-const putGoals = (req, res) => {
+const putGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `update individual goal ${req.params.id}` });
-};
+});
 
-const delGoals = (req, res) => {
+const delGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete individual goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
@@ -33,3 +32,6 @@ module.exports = {
 
 // ; to use body we need to add some middleware
 // req.body - post - body - x-www-form-urlencoded
+//async because it will return a promise when attached to mongodb
+// usually we use try catch block but here we are using npm package  - npm i express-async-handler
+//express-async-handler wrap controllers in it
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@
 
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -9,6 +11,8 @@ const app = express();
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(errorHandler);
 
 //Routing
 app.use('/api/goals', require('./routes/goalRoutes'));

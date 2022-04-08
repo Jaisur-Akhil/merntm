59ed798048191a3b086494e79d7cb33080b7d380
Akhil <77776342+Jaisur-Akhil@users.noreply.github.com>
2022-04-08 11:00:25 +0530

    get request

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,17 @@
 /** @format */
 
-console.log('hello Work I enjoyy');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get('/api/goals', (req, res) => {
+  //   res.send('Get Goals'); // Nomal
+  //   res.json({ message: 'get Goals' }); // Json format
+  res.json({ message: 'get Goals' }); // Json format
+  res.status(200).json({ message: 'get Goals' }); // Json format
+});
+
+app.listen(port, () => console.log(`Server started at ${port}`));
+
+// git commit -m 'Initial commit'
